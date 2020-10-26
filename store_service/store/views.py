b97e33eeb3c5ef4ef5d30c8f594e9dd688214f5a
ivasik <51726661+Blackblackofficial@users.noreply.github.com>
2020-10-26 05:51:06 +0300

    Settings Store service

--- store_service/store/views.py
+++ store_service/store/views.py
@@ -1,12 +1,8 @@
 from django.core.exceptions import ValidationError
 from django.http import JsonResponse
-from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
-
 from rest_framework.parsers import JSONParser
-from rest_framework.response import Response
-from rest_framework.views import APIView
 from .models import Store
 from .serializers import StoreSerializer
 import requests
@@ -21,17 +17,34 @@ def validUser(user_uid):
         return False
 
 
-class StoreView(APIView):
-    def get(self, request, user_uid):
-        if request.method == 'GET':
-            if validUser(user_uid):
-                store = Store.objects.all()
-                serializer = StoreSerializer(store, many=True)
-                # r = requests.get('https://peaceful-shelf-78026.herokuapp.com/persons')
-                # # persons = JSONParser().parse('https://peaceful-shelf-78026.herokuapp.com/persons')
-                return JsonResponse(serializer.data, safe=False)
-            else:
-                return JsonResponse({'message': 'The tutorial does not exist or No Content'},
-                                    status=status.HTTP_404_NOT_FOUND)
+@api_view(['GET'])
+def get_orders(request, user_uid):
+    if request.method == 'GET':
+        if validUser(user_uid):
+            store = Store.objects.all()
+            serializer = StoreSerializer(store, many=True)
+            # r = requests.get('https://peaceful-shelf-78026.herokuapp.com/persons')
+            # # persons = JSONParser().parse('https://peaceful-shelf-78026.herokuapp.com/persons')
+            return JsonResponse(serializer.data, safe=False)
         else:
-            return JsonResponse('', status=status.HTTP_405_METHOD_NOT_ALLOWED)
+            return JsonResponse({'message': 'The tutorial does not exist or No Content'},
+                                status=status.HTTP_404_NOT_FOUND)
+    else:
+        return JsonResponse('', status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+@api_view(['POST'])
+def buy_order(request, user_uid):
+    if request.method == 'POST':
+        if validUser(user_uid):
+            store = JSONParser().parse(request)
+            store = store
+        else:
+            return JsonResponse({'message': 'The tutorial does not exist or No Content'},
+                                status=status.HTTP_404_NOT_FOUND)
+    else:
+        return JsonResponse('', status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    # def post(self, request, user_uid, user):
+    #     store = JSONParser().parse(request)
+    #     store = store
