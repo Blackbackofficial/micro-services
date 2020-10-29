654d3d0d30b6022812f4c8d0b486c3249b0aa931
ivasik <51726661+Blackblackofficial@users.noreply.github.com>
2020-10-29 23:44:32 +0300

    Fix orders settings

--- order_service/order_service/settings.py
+++ order_service/order_service/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'orders.apps.OrdersConfig'
+    'orders.apps.OrdersConfig',
+    'rest_framework_swagger',
 ]
 
 REST_FRAMEWORK = {
