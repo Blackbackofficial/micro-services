1854ee5c4e6170b51e17c00ce709d02ab6d0e501
ivasik <51726661+Blackblackofficial@users.noreply.github.com>
2020-10-29 05:27:55 +0300

    Settings json api and django-openapi

--- store_service/store_service/urls.py
+++ store_service/store_service/urls.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include
+from django_openapi import OpenAPI
+from django_openapi import Path, Query, Form
+
+# create an API object instance
+api = OpenAPI(title='My OpenAPI Test', prefix_path='/test_api')
 
 API_VERSION = 'api/v1/'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(API_VERSION+'store/', include("store.urls")),
+    path(API_VERSION + 'store/', include("store.urls")),
     path('manage/health/', include('store.health_url')),
+    api.as_django_url_pattern()  # Add API object into urlpatterns
 ]
+
+
+@api.get('/test/hello_via_path/{word}', tags=['test'])
+def hello_via_path(word=Path()):
+    return {'hello': word}
