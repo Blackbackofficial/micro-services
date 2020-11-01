2e7889d7b6b173acc1789d6ae2704f18e8a9a513
ivasik <51726661+Blackblackofficial@users.noreply.github.com>
2020-11-01 06:36:55 +0300

    Add DELETE and GET method

--- order_service/orders/serializers.py
+++ order_service/orders/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
-from orders.models import Orders
+from .models import Orders
 
 
 class OrdersSerializer(serializers.ModelSerializer):
     class Meta:
         model = Orders
-        fields = ('id', 'item_uid', 'order_date', 'order_uid', 'status', 'user_uid')
+        fields = ('item_uid', 'order_date', 'order_uid', 'status')
--- order_service/orders/views.py
+++ order_service/orders/views.py
@@ -1,12 +1,13 @@
 from rest_framework.parsers import JSONParser
 
+from .models import Orders
 from .serializers import OrdersSerializer
 from django.http import JsonResponse
 from rest_framework import status
 from rest_framework.decorators import api_view
 
 
-@api_view(['POST'])
+@api_view(['GET', 'POST', 'DELETE'])
 def make_orders(request, user_uid):
     if request.method == 'POST':
         store_data = JSONParser().parse(request)
@@ -16,5 +17,17 @@ def make_orders(request, user_uid):
             order_serializer.save()
             # Логика
             return JsonResponse({"orderUid": order_serializer.data["order_uid"]}, status=status.HTTP_200_OK)
+    if request.method == 'GET':
+        orders = Orders.objects.all().filter(user_uid=user_uid)
+        orders_serializer = OrdersSerializer(orders, many=True)
+        return JsonResponse(orders_serializer.data, status=status.HTTP_200_OK, safe=False)
+    if request.method == 'DELETE':
+        try:
+            order_safe = Orders.objects.get(order_uid=user_uid)
+            order_safe.delete()
+            return JsonResponse(1, status=status.HTTP_204_NO_CONTENT, safe=False)
+        except Orders.DoesNotExist:
+            return JsonResponse({'message': 'Order {} not found'.format(user_uid)}, status=status.HTTP_404_NOT_FOUND)
     else:
         return JsonResponse('', status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
