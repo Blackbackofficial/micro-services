110ee75153848f215f1ef1f83e89c5b9cd6d128e
ivasik <51726661+Blackblackofficial@users.noreply.github.com>
2020-11-01 07:25:28 +0300

    Clean code views.py in store

--- store_service/store/views.py
+++ store_service/store/views.py
@@ -10,7 +10,6 @@ import requests
 
 def validUser(user_uid):
     try:
-        user_uid = str(user_uid)
         Store.objects.get(user_uid=user_uid)
         return True
     except ValidationError:
@@ -19,76 +18,58 @@ def validUser(user_uid):
 
 @api_view(['GET'])
 def get_orders(request, user_uid):
-    if request.method == 'GET':
-        if validUser(user_uid):
-            # Логика
-            store = Store.objects.all()
-            serializer = StoreSerializer(store, many=True)
-            # r = requests.get('https://peaceful-shelf-78026.herokuapp.com/persons')
-            # # persons = JSONParser().parse('https://peaceful-shelf-78026.herokuapp.com/persons')
-            return JsonResponse(serializer.data, safe=False)
-        else:
-            return JsonResponse({'message': 'The tutorial does not exist or No Content'},
-                                status=status.HTTP_404_NOT_FOUND)
+    if validUser(user_uid):
+        # Логика
+        store = Store.objects.all()
+        serializer = StoreSerializer(store, many=True)
+        # r = requests.get('https://peaceful-shelf-78026.herokuapp.com/persons')
+        # # persons = JSONParser().parse('https://peaceful-shelf-78026.herokuapp.com/persons')
+        return JsonResponse(serializer.data, safe=False)
     else:
-        return JsonResponse('', status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        return JsonResponse({'message': 'The tutorial does not exist or No Content'}, status=status.HTTP_404_NOT_FOUND)
 
 
 @api_view(['POST'])
 def buy_order(request, user_uid):
-    if request.method == 'POST':
-        if validUser(user_uid):
-            order_req = requests.post('http://127.0.0.1:8100/api/v1/orders/{}'.format(user_uid), json=request.data)
-            order_req = order_req.json()
-            response = JsonResponse(1, status=status.HTTP_201_CREATED, safe=False)
-            response['Location'] = 'http://127.0.0.1:8100/api/v1/orders/{user_uid}/purchase/{order_uid}'.format(
-                user_uid=user_uid, order_uid=order_req["orderUid"])
-            return response
-        else:
-            return JsonResponse({'message': 'The tutorial does not exist or No Content'},
-                                status=status.HTTP_404_NOT_FOUND)
+    if validUser(user_uid):
+        order_req = requests.post('http://127.0.0.1:8100/api/v1/orders/{}'.format(user_uid), json=request.data)
+        order_req = order_req.json()
+        response = JsonResponse(1, status=status.HTTP_201_CREATED, safe=False)
+        response['Location'] = 'http://127.0.0.1:8100/api/v1/orders/{user_uid}/purchase/{order_uid}'.format(
+            user_uid=user_uid, order_uid=order_req["orderUid"])
+        return response
     else:
-        return JsonResponse('', status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        return JsonResponse({'message': 'The tutorial does not exist or No Content'}, status=status.HTTP_404_NOT_FOUND)
 
 
 @api_view(['GET'])
 def get_order(request, user_uid, order_uid):
-    if request.method == 'GET':
-        if validUser(user_uid):
-            # Логика
-            store = JSONParser().parse(request)
-            store = store
-        else:
-            return JsonResponse({'message': 'The tutorial does not exist or No Content'},
-                                status=status.HTTP_404_NOT_FOUND)
+    if validUser(user_uid):
+        # Логика
+        store = JSONParser().parse(request)
+        store = store
     else:
-        return JsonResponse('', status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        return JsonResponse({'message': 'The tutorial does not exist or No Content'}, status=status.HTTP_404_NOT_FOUND)
 
 
 @api_view(['POST'])
 def get_order_warranty(request, user_uid, order_uid):
-    if request.method == 'POST':
-        if validUser(user_uid):
-            # Логика
-            store = JSONParser().parse(request)
-            store = store
-        else:
-            return JsonResponse({'message': 'The tutorial does not exist or No Content'},
-                                status=status.HTTP_404_NOT_FOUND)
+    if validUser(user_uid):
+        # Логика
+        store = JSONParser().parse(request)
+        store = store
     else:
-        return JsonResponse('', status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        return JsonResponse({'message': 'The tutorial does not exist or No Content'}, status=status.HTTP_404_NOT_FOUND)
 
 
 # Только метод DELETE
-@api_view(['POST'])
+@api_view(['DELETE'])
 def get_order_refund(request, user_uid, order_uid):
-    if request.method == 'POST':
-        if validUser(user_uid):
-            # Логика
-            store = JSONParser().parse(request)
-            store = store
-        else:
-            return JsonResponse({'message': 'The tutorial does not exist or No Content'},
-                                status=status.HTTP_404_NOT_FOUND)
+    if validUser(user_uid):
+        order_req = requests.delete('http://127.0.0.1:8100/api/v1/orders/{}'.format(order_uid), json=request.data)
+        if order_req.status_code == 404:
+            order_req = order_req.json()
+            return JsonResponse(order_req, status=status.HTTP_404_NOT_FOUND, safe=False)
+        return JsonResponse(1, status=status.HTTP_204_NO_CONTENT, safe=False)
     else:
-        return JsonResponse('', status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        return JsonResponse({'message': 'The tutorial does not exist or No Content'}, status=status.HTTP_404_NOT_FOUND)
