bc815614b8e371889a29c6d79bba3f124db8f15b
ivasik <51726661+Blackblackofficial@users.noreply.github.com>
2020-11-03 18:56:43 +0300

    1)Clean code 2)Renaming variables 3)Something fix

--- warehouse_service/warehouse/urls.py
+++ warehouse_service/warehouse/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('<str:orderItemUid>/warranty', views.warranty_solution),
     path('', views.post_items),
     path('<str:orderItemUid>', views.request_items),
     path('<str:orderItemUid>/warranty', views.request_warranty)
--- warehouse_service/warehouse/views.py
+++ warehouse_service/warehouse/views.py
@@ -12,60 +12,62 @@ import requests
 def post_items(request):
     parseDict = JSONParser().parse(request)
     try:
-        req_data = req_items = Items.objects.get(model=parseDict['model'])
-        req_data = ItemSerializer(req_data).data
-        req_data['available_count'] -= 1
-        if req_data['available_count'] == 0:
+        instData = instItem = Items.objects.get(model=parseDict['model'])
+        instData = ItemSerializer(instData).data
+        instData['available_count'] -= 1
+        if instData['available_count'] == 0:
             return JsonResponse({'message': 'Item not available'}, status=status.HTTP_409_CONFLICT)
-        req_serializer = ItemSerializer(instance=req_items, data=req_data)
+        req_serializer = ItemSerializer(instance=instItem, data=instData)
         if req_serializer.is_valid():
             req_serializer.save()
-        order_item = dict(order_uid=parseDict['orderUid'], item_id=req_data['id'])
-        item_serializer = OrderItemSerializer(data=order_item)
+        orderItem = dict(order_uid=parseDict['orderUid'], item_id=instData['id'])
+        item_serializer = OrderItemSerializer(data=orderItem)
         if item_serializer.is_valid():
             item_serializer.save()
         order = Order_item.objects.get(order_uid=parseDict['orderUid']).order_item_uid
         req = dict(orderItemUid=order, orderUid=parseDict['orderUid'], model=parseDict['model'], size=parseDict['size'])
         return JsonResponse(req, status=status.HTTP_200_OK)
     except Items.DoesNotExist:
-        return JsonResponse({'message': 'Requested item not found'}, status=status.HTTP_404_NOT_FOUND)
-
-
-@api_view(['GET', 'DELETE'])
-def request_items(request, orderItemUid):
-    try:
-        startOrder = order = Order_item.objects.get(order_item_uid=orderItemUid)
-        startData = req_data = Items.objects.get(id=order.item_id)
-        req_data = ItemSerializer(req_data).data
-    except Order_item.DoesNotExist:
-        return JsonResponse({'message': 'Order item not found'}, status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        del req_data['id'], req_data['available_count']
-        return JsonResponse(req_data, status=status.HTTP_200_OK, safe=False)
-    if request.method == 'DELETE':
-        req_data['available_count'] += 1
-        req_serializer = ItemSerializer(instance=startData, data=req_data)
-        if req_serializer.is_valid():
-            req_serializer.save()
-        order = OrderItemSerializer(order).data
-        order['canceled'] = True
-        order_serializer = OrderItemSerializer(instance=startOrder, data=order)
-        if order_serializer.is_valid():
-            order_serializer.save()
-        return JsonResponse(1, status=status.HTTP_204_NO_CONTENT, safe=False)
+        return JsonResponse({'message': 'Requested \'{}\' not find'.format(parseDict['orderUid'])},
+                            status=status.HTTP_404_NOT_FOUND)
 
 
 @api_view(['POST'])
 def request_warranty(request, orderItemUid):
     parseDict = JSONParser().parse(request)
     try:
-        item_id = Order_item.objects.get(order_item_uid=orderItemUid).item_id
-        available_count = Items.objects.get(id=item_id).available_count
-        resJson = dict(available_count=available_count, reason=parseDict['reason'])
-        warranty_req = requests.post('http://127.0.0.1:8200/api/v1/warranty/{}/warranty'.format(orderItemUid), json=resJson)
-        if warranty_req.status_code == 404:
-            return JsonResponse({'message': 'Warranty not found for itemUid \'{}\''.format(orderItemUid)}, status=status.HTTP_404_NOT_FOUND)
-        return JsonResponse(warranty_req.json(), status=status.HTTP_200_OK)
+        orderItem = Order_item.objects.get(order_item_uid=orderItemUid).item_id
+        availableCount = Items.objects.get(id=orderItem).available_count
+        resJson = dict(available_count=availableCount, reason=parseDict['reason'])
+        requestW = requests.post('http://127.0.0.1:8200/api/v1/warranty/{}/warranty'.format(orderItemUid), json=resJson)
+        if requestW.status_code == 404:
+            return JsonResponse({'message': 'Warranty not found for itemUid \'{}\''.format(orderItemUid)},
+                                status=status.HTTP_404_NOT_FOUND)
+        return JsonResponse(requestW.json(), status=status.HTTP_200_OK)
     except Order_item.DoesNotExist:
-        return JsonResponse({'message': 'Order item not found'}, status=status.HTTP_404_NOT_FOUND)
+        return JsonResponse({'message': 'Order \'{}\' not find'.format(orderItemUid)}, status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['GET', 'DELETE'])
+def request_items(request, orderItemUid):
+    try:
+        instOrder = orderItem = Order_item.objects.get(order_item_uid=orderItemUid)
+        instData = items = Items.objects.get(id=orderItem.item_id)
+        items = ItemSerializer(items).data
+    except Order_item.DoesNotExist:
+        return JsonResponse({'message': 'Order \'{}\' not find'.format(orderItemUid)}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        del items['id'], items['available_count']
+        return JsonResponse(items, status=status.HTTP_200_OK, safe=False)
+    if request.method == 'DELETE':
+        items['available_count'] += 1
+        data_serializer = ItemSerializer(instance=instData, data=items)
+        if data_serializer.is_valid():
+            data_serializer.save()
+        orderItem = OrderItemSerializer(orderItem).data
+        orderItem['canceled'] = True
+        order_serializer = OrderItemSerializer(instance=instOrder, data=orderItem)
+        if order_serializer.is_valid():
+            order_serializer.save()
+        return JsonResponse(1, status=status.HTTP_204_NO_CONTENT, safe=False)
