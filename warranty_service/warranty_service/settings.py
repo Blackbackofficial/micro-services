d1b6e0b4b88684289de5527194cce1d53ed87a1c
ivasik <51726661+Blackblackofficial@users.noreply.github.com>
2020-11-04 23:46:40 +0300

    Deploy

--- warranty_service/warranty_service/settings.py
+++ warranty_service/warranty_service/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'warranty',
+    'warranty.apps.WarrantyConfig',
     'rest_framework_swagger',
 ]
 
