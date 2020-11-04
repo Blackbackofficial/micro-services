9f3ffff30c9c381ce0501dc405b04cd7ac571587
ivasik <51726661+Blackblackofficial@users.noreply.github.com>
2020-11-04 03:14:48 +0300

    Add API Orders & add README files in all services

--- order_service/orders/urls.py
+++ order_service/orders/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('<str:order_uid>/warranty', views.warranty_order),
     path('<str:user_uid>', views.actions_orders),
     path('<str:user_uid>/<str:order_uid>', views.one_order),
-    path('<str:user_uid>/<str:order_uid>/warranty', views.warranty_order)
+    # path('<str:order_uid>/warranty', views.warranty_order)
 ]
